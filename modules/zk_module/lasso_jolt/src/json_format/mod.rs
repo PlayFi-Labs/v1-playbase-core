05655160ce964970f1cda1478568845d3aa187b9
eludius18 <eladio.robcas@gmail.com>
2024-08-15 12:26:52 +0100

    refactoring mod.rs

--- modules/zk_module/lasso_jolt/src/json_format/mod.rs
+++ modules/zk_module/lasso_jolt/src/json_format/mod.rs
@@ -1,4 +1,2 @@
 pub mod json_format;
-pub use json_format::JsonRecords;
-pub use json_format::u32_to_array;
-pub use json_format::array_to_u32;
+pub use json_format::{JsonRecords, u32_to_array, array_to_u32};
